10acafeb8fd52382e2937eeee37ee4a64e6af829
ken.lebot <ken.lebot@cgi.com>
2020-03-02 15:11:34 +0100

    jacoco

--- src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -3,6 +3,8 @@ package com.ipiecoles.java.java350.model;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
+import java.time.LocalDate;
+
 public class EmployeTest {
 
     @Test
@@ -16,5 +18,31 @@ public class EmployeTest {
 
         //Then = Vérifications de ce qu'a fait la méthode
         Assertions.assertThat(nombre).isEqualTo(0);
+    }
+
+    @Test
+    public void testgetNombreAnneeAncienneteTodayPlus2Y() {
+        //Given
+        Employe employe = new Employe();
+        employe.setDateEmbauche(LocalDate.now().plusYears(2));
+
+        //When = Exécution de la méthode à tester
+        Integer nombre = employe.getNombreAnneeAnciennete();
+
+        //Then = Vérifications de ce qu'a fait la méthode
+        Assertions.assertThat(nombre).isEqualTo(0);
+    }
+
+    @Test
+    public void testgetNombreAnneeAncienneteTodayMinus3Y() {
+        //Given
+        Employe employe = new Employe();
+        employe.setDateEmbauche(LocalDate.now().minusYears(3));
+
+        //When = Exécution de la méthode à tester
+        Integer nombre = employe.getNombreAnneeAnciennete();
+
+        //Then = Vérifications de ce qu'a fait la méthode
+        Assertions.assertThat(nombre).isEqualTo(3);
     }
 }
